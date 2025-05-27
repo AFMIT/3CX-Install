518875741dd7088ca001c2b0dd8e361d96ad7df1
AFMIT <armando.fernandez.mora@gmail.com>
2025-05-27 00:22:24 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "STARTING DEBIAN UPDATES"
+apt update && apt upgrade -y
+echo "DOWNLOADING SWAP FILES"
+apt install sudo wget gnupg2 dphys-swapfile -y
+echo "DOWLOADING PUBLIC KEY"
+wget -O- https://repo.3cx.com/key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/3cx-archive-keyring.gpg > /dev/nullecho "DOWNLOADING REPOSITORIES"
+echo "deb [arch=amd64 by-hash=yes signed-by=/usr/share/keyrings/3cx-archive-keyring.gpg] http://repo.3cx.com/3cx bookwo>echo "Updating all files"
+apt update
+echo "DOWNLOADING 3CX"
+apt install 3cxpbx -y
